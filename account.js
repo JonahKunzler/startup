e5b9007da69637dbbfb21f86fa12eccff8566ff4
JonahKunzler <jonahk12@gmail.com>
2023-03-11 19:49:39 -0700

    Making functions where you can input your name, email, and password for account creation/sign in

---
+++ account.js
@@ -0,0 +1,40 @@
+function createAccount() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+  
+    if (name === "" || email === "" || password === "") {
+      document.getElementById("message").innerHTML = "Please fill in all fields";
+      return;
+    }
+
+    let account = {
+      name: name,
+      email: email,
+      password: password
+    }
+  
+    localStorage.setItem(email, JSON.stringify(account));
+  
+    document.getElementById("message").innerHTML = "Account created successfully";
+  }
+  
+  function login() {
+    let email = document.getElementById("login-email").value;
+    let password = document.getElementById("login-password").value;
+  
+    if (email === "" || password === "") {
+      document.getElementById("login-message").innerHTML = "Please fill in all fields";
+      return;
+    }
+  
+    let account = JSON.parse(localStorage.getItem(email));
+  
+    if (account !== null && account.password === password) {
+      document.getElementById("login-message").innerHTML = "Login successful";
+      window.location.href = "dashboard.html";
+    } else {
+      document.getElementById("login-message").innerHTML = "Invalid email or password";
+    }
+  }
+  
