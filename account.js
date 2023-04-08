ef20313f3c9fe4504f37009850a8f4d02f7da962
JonahKunzler <jonahk12@gmail.com>
2023-04-07 22:28:36 -0600

    Had to make sure all the functions were called on peerProxy

--- account.js
+++ account.js
@@ -1,40 +1,96 @@
-function createAccount() {
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-  
-    if (name === "" || email === "" || password === "") {
-      document.getElementById("message").innerHTML = "Please fill in all fields";
-      return;
-    }
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const express = require('express');
+const app = express();
+const DB = require('./database.js');
+const { peerProxy } = require('./peerProxy.js');
 
-    let account = {
-      name: name,
-      email: email,
-      password: password
-    }
-  
-    localStorage.setItem(email, JSON.stringify(account));
-  
-    document.getElementById("message").innerHTML = "Account created successfully";
+const authCookieName = 'token';
+
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
+// Serve up the applications static content
+app.use(express.static('public'));
+
+// Router for service endpoints
+const apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// CreateAuth token for a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await DB.getUser(req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await DB.createUser(req.body.email, req.body.password);
+
+    
+    setAuthCookie(res, user.token);
+
+    res.send({
+      id: user._id,
+    });
   }
-  
-  function login() {
-    let email = document.getElementById("login-email").value;
-    let password = document.getElementById("login-password").value;
-  
-    if (email === "" || password === "") {
-      document.getElementById("login-message").innerHTML = "Please fill in all fields";
+});
+
+
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await DB.getUser(req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
       return;
     }
-  
-    let account = JSON.parse(localStorage.getItem(email));
-  
-    if (account !== null && account.password === password) {
-      document.getElementById("login-message").innerHTML = "Login successful";
-      window.location.href = "dashboard.html";
-    } else {
-      document.getElementById("login-message").innerHTML = "Invalid email or password";
-    }
   }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// GetUser returns information about a user
+apiRouter.get('/user/:email', async (req, res) => {
+  const user = await DB.getUser(req.params.email);
+  if (user) {
+    const token = req?.cookies.token;
+    res.send({ email: user.email, authenticated: token === user.token });
+    return;
+  }
+  res.status(404).send({ msg: 'Unknown' });
+});
+
+// secureApiRouter verifies credentials for endpoints
+const secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  const authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
+
+app.use((_req, res) => {
+  res.sendFile('home.html', { root: 'public' });
+});
+
+const httpService = app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+
   
+peerProxy(httpService);
